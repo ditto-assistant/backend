28b29ff7702c0bb305966635b859a228c47fa324
Peyton-Spencer <pas1234567@gmail.com>
2024-09-29 23:25:33 -0400

    update example tables

--- pkg/exstore/create-example-store.sql
+++ pkg/exstore/create-example-store.sql
@@ -1,17 +1,17 @@
 CREATE TABLE IF NOT EXISTS tools (
-  id TEXT PRIMARY KEY,
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
   name TEXT,
   description TEXT,
-  version TEXT,
+  version TEXT
 );
 
 CREATE TABLE IF NOT EXISTS examples (
-  id TEXT PRIMARY KEY,
-  tool_id TEXT,
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  tool_id INTEGER,
   prompt TEXT,
   response TEXT,
   -- 768-dimensional f32 vector embeddings
   em_prompt F32_BLOB(768), 
   em_response F32_BLOB(768), 
-  em_prompt_response F32_BLOB(768),
+  em_prompt_response F32_BLOB(768)
 );
