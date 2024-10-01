4e64b6305e507a80e236dff2734ac7581aeeeca4
Peyton-Spencer <pas1234567@gmail.com>
2024-10-01 00:05:53 -0400

    working turso conn

--- pkg/db/database.go
+++ pkg/db/database.go
@@ -11,7 +11,6 @@ import (
 
 	"github.com/ditto-assistant/backend/pkg/envs"
 	"github.com/ditto-assistant/backend/pkg/secr"
-	"github.com/firebase/genkit/go/ai"
 	"github.com/tursodatabase/go-libsql"
 )
 
@@ -39,6 +38,11 @@ func Setup(ctx context.Context, shutdown *sync.WaitGroup) error {
 
 	db = sql.OpenDB(connector)
 	slog.Debug("db connected", "url", envs.DB_URL_DITTO_EXAMPLES)
+
+	if err := migrate(ctx); err != nil {
+		return fmt.Errorf("error creating tables: %w", err)
+	}
+
 	go func() {
 		<-ctx.Done()
 		slog.Debug("shutting down libsql db")
@@ -48,18 +52,63 @@ func Setup(ctx context.Context, shutdown *sync.WaitGroup) error {
 		shutdown.Done()
 	}()
 
-	ai.DefineIndexer("custom", "example-indexer", func(ctx context.Context, ir *ai.IndexerRequest) error {
-		return nil
-	})
+	// ai.DefineIndexer("custom", "example-indexer", func(ctx context.Context, ir *ai.IndexerRequest) error {
+	// 	return nil
+	// })
 
 	return nil
 }
 
-func createTables(ctx context.Context) error {
-	return nil
+func migrate(ctx context.Context) error {
+	// Check if the migrations table exists
+	var tableExists bool
+	var name string
+	err := db.QueryRowContext(ctx, "SELECT name FROM sqlite_master WHERE type='table' AND name='migrations'").Scan(&name)
+	tableExists = (name == "migrations")
+	if err != nil && err != sql.ErrNoRows {
+		return fmt.Errorf("error checking if migrations table exists: %w", err)
+	}
+	if !tableExists {
+		slog.Debug("migrations table does not exist, running initial migration")
+	} else {
+		// Check if we have at least one migration
+		var count int
+		err = db.QueryRowContext(ctx, "SELECT COUNT(*) FROM migrations").Scan(&count)
+		if err != nil {
+			return fmt.Errorf("error checking migrations: %w", err)
+		} else if count > 0 {
+			slog.Debug("tables already migrated", "migrations", count)
+			return nil
+		} else {
+			slog.Debug("migrations table exists but no migrations found")
+		}
+	}
+
+	tx, err := db.BeginTx(ctx, nil)
+	if err != nil {
+		return fmt.Errorf("error beginning transaction: %w", err)
+	}
+	defer tx.Rollback()
+	// Create the initial migration
+	_, err = tx.ExecContext(ctx, createExampleStore)
+	if err != nil {
+		return fmt.Errorf("error creating tables: %w", err)
+	}
+	slog.Debug("tables migrated")
+	return tx.Commit()
 }
 
 const createExampleStore = `
+CREATE TABLE IF NOT EXISTS migrations (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  migration_name TEXT,
+  migration_date TEXT DEFAULT (datetime('now'))
+);
+INSERT INTO migrations (migration_name) 
+SELECT 'table_init' 
+WHERE NOT EXISTS (SELECT 1 FROM migrations);
+
+
 CREATE TABLE IF NOT EXISTS tools (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   name TEXT,
