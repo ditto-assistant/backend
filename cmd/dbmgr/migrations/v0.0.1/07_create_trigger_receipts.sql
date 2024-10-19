5cb9a4931ff4352fbf6794ef983ea4f254c41894
Peyton-Spencer <pas1234567@gmail.com>
2024-10-19 05:54:28 -0400

    ditto token price table

--- cmd/dbmgr/migrations/v0.0.1/07_create_trigger_receipts.sql
+++ cmd/dbmgr/migrations/v0.0.1/07_create_trigger_receipts.sql
@@ -7,19 +7,19 @@ BEGIN
     UPDATE receipts
     SET ditto_token_cost = (
         SELECT MAX(1, ROUND(
-            (COALESCE(base_cost_per_call, 0) * 1000000 +
-             COALESCE(base_cost_per_million_tokens * (NEW.total_tokens / 1000000.0), 0) * 1000000 +
-             COALESCE(base_cost_per_million_input_tokens * (NEW.input_tokens / 1000000.0), 0) * 1000000 +
-             COALESCE(base_cost_per_million_output_tokens * (NEW.output_tokens / 1000000.0), 0) * 1000000 +
-             COALESCE(base_cost_per_image * NEW.num_images, 0) * 1000000 +
-             COALESCE(base_cost_per_search * NEW.num_searches, 0) * 1000000 +
-             COALESCE(base_cost_per_second * NEW.call_duration_seconds, 0) * 1000000 +
-             COALESCE(base_cost_per_gb_processed * (NEW.data_processed_bytes / 1073741824.0), 0) * 1000000 +
-             COALESCE(base_cost_per_gb_stored * (NEW.data_stored_bytes / 1073741824.0), 0) * 1000000
+            (COALESCE(base_cost_per_call, 0) * dtpd.count +
+             COALESCE(base_cost_per_million_tokens * (NEW.total_tokens / 1000000.0), 0) * dtpd.count +
+             COALESCE(base_cost_per_million_input_tokens * (NEW.input_tokens / 1000000.0), 0) * dtpd.count +
+             COALESCE(base_cost_per_million_output_tokens * (NEW.output_tokens / 1000000.0), 0) * dtpd.count +
+             COALESCE(base_cost_per_image * NEW.num_images, 0) * dtpd.count +
+             COALESCE(base_cost_per_search * NEW.num_searches, 0) * dtpd.count +
+             COALESCE(base_cost_per_second * NEW.call_duration_seconds, 0) * dtpd.count +
+             COALESCE(base_cost_per_gb_processed * (NEW.data_processed_bytes / 1073741824.0), 0) * dtpd.count +
+             COALESCE(base_cost_per_gb_stored * (NEW.data_stored_bytes / 1073741824.0), 0) * dtpd.count
             ) * (1 + profit_margin_percentage / 100.0)
         ))
-        FROM services
-        WHERE id = NEW.service_id
+        FROM services, tokens_per_dollar AS dtpd
+        WHERE services.id = NEW.service_id AND dtpd.name = 'ditto'
     )
     WHERE id = NEW.id;
     -- Update the user's balance
