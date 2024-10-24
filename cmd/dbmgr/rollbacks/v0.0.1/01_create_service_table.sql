d80b5770296e3a03886a4a6b20a40f42ccdaddaf
Peyton Spencer <pas1234567@gmail.com>
2024-10-23 20:45:47 -0400

    Staging (#37)
    * dbmgr fixes
    
    * bump up the max tokens
    
    * delete old rollback files

--- cmd/dbmgr/rollbacks/v0.0.1/01_create_service_table.sql
+++
@@ -1,3 +0,0 @@
-DROP TABLE IF EXISTS services;
-
-DROP TRIGGER IF EXISTS update_services_timestamp;
--- cmd/dbmgr/rollbacks/v0.0.1/06.5_create_ditto_price_table.sql
+++
@@ -1,3 +0,0 @@
-DROP TABLE IF EXISTS tokens_per_dollar;
-
-DROP TRIGGER IF EXISTS update_tokens_per_dollar_timestamp;
