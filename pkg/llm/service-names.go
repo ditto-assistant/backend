fcf132e0f0996633528ece3a77ba274d8cdab44c
Peyton-Spencer <pas1234567@gmail.com>
2024-11-04 23:21:55 -0600

    Llama implemented

--- pkg/llm/service-names.go
+++ pkg/llm/service-names.go
@@ -22,6 +22,8 @@ const (
 	ModelDalle3 ServiceName = "dall-e-3"
 	// ModelMistralNemo is Mistral's nemo model.
 	ModelMistralNemo ServiceName = "mistral-nemo"
+	// ModelLlama32Vision is Meta's Llama 3.2 Vision model.
+	ModelLlama32Vision ServiceName = "llama-3.2-vision"
 )
 
 // Search Engines
