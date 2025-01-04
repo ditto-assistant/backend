1219a2508e43bc5c4f4e28a91b81b5c00876114a
Peyton-Spencer <pas1234567@gmail.com>
2025-01-03 20:51:08 -0500

    v2 get-memories params

---
+++ pkg/fbase/memories-firebase.go
@@ -0,0 +1,11 @@
+package fbase
+
+import "context"
+
+func (a *FirebaseApp) GetMemoriesShortTerm(ctx context.Context, userID string) ([]string, error) {
+	return nil, nil
+}
+
+func (a *FirebaseApp) GetMemoriesLongTerm(ctx context.Context, userID string) ([]string, error) {
+	return nil, nil
+}
