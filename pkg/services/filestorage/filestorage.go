bcf9d25173a6b88d4f33338018d1e08a1cd8ebbf
Peyton Spencer <pas1234567@gmail.com>
2025-03-07 01:43:45 -0500

    do not presign if the dalle image has not expired (#205)

--- pkg/services/filestorage/filestorage.go
+++ pkg/services/filestorage/filestorage.go
@@ -6,6 +6,8 @@ import (
 	"strings"
 	"time"
 
+	"net/url"
+
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
@@ -49,11 +51,20 @@ func NewClient(ctx context.Context) (*Client, error) {
 	return cl, nil
 }
 
-func (cl *Client) PresignURL(ctx context.Context, userID, url string) (string, error) {
-	return cl.urlCache.Get(url, func() (string, error) {
-		urlParts := strings.Split(url, "?")
+func (cl *Client) PresignURL(ctx context.Context, userID, urlStr string) (string, error) {
+	if strings.Contains(urlStr, "oaidalleapiprodscus.blob.core.windows.net") {
+		valid, err := checkAzureStillValid(urlStr)
+		if err != nil {
+			return "", err
+		}
+		if valid {
+			return urlStr, nil
+		}
+	}
+	return cl.urlCache.Get(urlStr, func() (string, error) {
+		urlParts := strings.Split(urlStr, "?")
 		if len(urlParts) == 0 {
-			return "", fmt.Errorf("failed to get filename from URL: %s", url)
+			return "", fmt.Errorf("failed to get filename from URL: %s", urlStr)
 		}
 		filename := strings.TrimPrefix(urlParts[0], envs.DITTO_CONTENT_PREFIX)
 		filename = strings.TrimPrefix(filename, envs.DALL_E_PREFIX)
@@ -67,4 +78,20 @@ func (cl *Client) PresignURL(ctx context.Context, userID, url string) (string, e
 		objReq, _ := cl.S3.GetObjectRequest(input)
 		return objReq.Presign(presignTTL)
 	})
+}
+
+func checkAzureStillValid(urlStr string) (bool, error) {
+	parsedURL, err := url.Parse(urlStr)
+	if err != nil {
+		return false, fmt.Errorf("failed to parse DALL-E URL: %w", err)
+	}
+	expiryParam := parsedURL.Query().Get("se")
+	if expiryParam == "" {
+		return false, fmt.Errorf("no expiry date found in DALL-E URL: %s", urlStr)
+	}
+	expiryDate, err := time.Parse(time.RFC3339, expiryParam)
+	if err != nil {
+		return false, fmt.Errorf("failed to parse expiry date: %w", err)
+	}
+	return time.Now().UTC().Before(expiryDate), nil
 }
